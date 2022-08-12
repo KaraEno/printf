bf7d049314e196a0cea8154c6b6cb472728fe45f
KaraEno <abasiakaraalexander@gmail.com>
2022-08-12 10:25:55 -0700

    Print function

--- _printf.c
+++ _printf.c
@@ -1,50 +1,94 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
-* _printf - print out values
-* @format: format
-* @...: variables
-* Return: returns the length
-*/
+ * printSpecifiers - prints special characters
+ * @next: character after the %
+ * @arg: argument for the indentifier
+ * Return: the number of characters printed
+ * (excluding the null byte used to end output to strings)
+ */
+
+int printSpecifiers(char next, va_list arg)
+{
+	int index;
+
+	specifiers utils[] = {
+		{"c", print_char},
+		{"s", print_str},
+		{"d", print_int},
+		{"i", print_int},
+		{"u", print_unsignedInt},
+		{"b", print_unsignedToBinary},
+		{"o", print_oct},
+		{"x", print_hex},
+		{"X", print_HEX},
+		{"S", print_STR},
+		{"r", rev_string},
+		{"R", rot13},
+		{NULL, NULL}
+	};
+
+	for (index = 0; utils[index].specifier != NULL; index++)
+	{
+		if (utils[index].specifier[0] == next)
+			return (utils[index].print(arg));
+	}
+	return (0);
+}
+
+/**
+ * _printf - mimic printf from stdio
+ * Description: produces output according to a format
+ * write output to stdout, the standard output stream
+ * @format: character string composed of zero or more directives
+ *
+ * Return: the number of characters that were printed
+ * (excluding the null byte used to end output to strings)
+ * return -1 for incomplete identifier error
+ */
+
 int _printf(const char *format, ...)
 {
-	int i = 0;
-	int length = 0;
-	va_list args;
+	unsigned int i;
+	int printSpecifier = 0, printChar = 0;
+	va_list arg;
 
-	va_start(args, format);
-	for (; format[i] != '\0'; i++)
+	va_start(arg, format);
+	if (format == NULL)
+		return (-1);
+
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		for (; format[i] == '%'; i++)
-		{
-			if (format[i + 1] == 'c')
-			{
-				length = length + _print_char(args);
-				i = i + 1;
-			}
-			else if (format[i + 1] == '%')
-			{
-				_putchar('%');
-				length++;
-				i = i + 1;
-			}
-			else if (format[i + 1] == 's')
-			{
-				length = length + _print_string(args);
-				i = i + 1;
-			}
-			else
-			{
-				_putchar(format[i]);
-				_putchar(format[i + 1]);
-				length++;
-				i = i + 1;
-			}
-		}
-		if (format[i])
+		if (format[i] != '%')
 		{
 			_putchar(format[i]);
-			length++;
+			printChar++;
+			continue;
+		}
+		if (format[i + 1] == '%')
+		{
+			_putchar('%');
+			printChar++;
+			i++;
+			continue;
+		}
+		if (format[i + 1] == '\0')
+			return (-1);
+
+		printSpecifier = printSpecifiers(format[i + 1], arg);
+		if (printSpecifier == -1 || printSpecifier != 0)
+			i++;
+		if (printSpecifier > 0)
+			printChar += printSpecifier;
+
+		if (printSpecifier == 0)
+		{
+			_putchar('%');
+			printChar++;
 		}
 	}
-	return (length);
+	va_end(arg);
+	return (printChar);
 }
--- main.h
+++ main.h
@@ -1,9 +1,35 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
 
-int _putchar(char);
+int _putchar(char c);
+int print_int(va_list arg);
+int print_unsignedInt(va_list arg);
 int _printf(const char *format, ...);
-int _print_char(va_list);
-int _print_string(va_list);
+int print_char(va_list arg);
+int print_str(va_list arg);
+int print_percent(void);
+void print_binary(unsigned int n, unsigned int *printed);
+int print_unsignedToBinary(va_list arg);
+int print_oct(va_list arg);
+int print_unsignedIntToHex(unsigned int num, char _case);
+int print_hex_base(va_list arg, char _case);
+int print_hex(va_list arg);
+int print_HEX(va_list arg);
+int print_STR(va_list arg);
+int  rev_string(va_list arg);
+int rot13(va_list arg);
+
+/**
+ * struct specifiers - defingin specifier struct type for print
+ * * @specifier: type
+ * @print: function to print
+ */
+typedef struct specifiers
+{
+char *specifier;
+int (*print)(va_list);
+} specifiers;
+
 #endif
